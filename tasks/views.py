b30074f1026af8e5611e9baa16e503818891b46f
Caleb <calebiramos@gmail.com>
2024-08-05 19:31:31 -0600

    FIN

--- tasks/views.py
+++ tasks/views.py
@@ -6,6 +6,8 @@ from django.contrib.auth import login, logout, authenticate
 from django.db import IntegrityError
 from .forms import TaskForm
 from .models import Task
+from django.utils import timezone
+from django.contrib.auth.decorators import login_required
 
 def home(request):
     return render(request, 'home.html')
@@ -32,12 +34,20 @@ def signup(request):
             "error": "Contraseña no coincide"
         })
 
+@login_required
 def tasks(request):
     tasks = Task.objects.filter(user=request.user, datecompleted__isnull=True)
     
     
     return render(request, 'tasks.html',{'tasks': tasks})
 
+@login_required
+def tasks_completed(request):
+    tasks = Task.objects.filter(user=request.user, datecompleted__isnull=False).order_by
+    ('-datecompleted')
+    return render(request, 'tasks.html',{'tasks': tasks})
+
+@login_required
 def create_tasks(request):
     if request.method == 'GET':
         return render(request, 'create_task.html',{
@@ -56,7 +66,8 @@ def create_tasks(request):
                 'error': 'Ingrese datos correctos'
         })
             
-
+            
+@login_required
 def task_detail(request, task_id):
     if request.method == 'GET' :
         task = get_object_or_404(Task,pk=task_id, user=request.user)
@@ -70,8 +81,24 @@ def task_detail(request, task_id):
             return redirect('tasks')
         except ValueError:
             return render(request, 'task_detail.html', {'task': task, 'form': form, 'error' :"Error al actualizar"})
+        
+        
+@login_required
+def complete_task(request, task_id):
+    task= get_object_or_404(Task, pk= task_id, user=request.user)
+    if request.method == 'POST':
+        task.datecompleted = timezone.now()
+        task.save()
+        return redirect('tasks')
+    
+@login_required    
+def delete_task(request, task_id):
+    task= get_object_or_404(Task, pk= task_id, user=request.user)
+    if request.method == 'POST':
+        task.delete()
+        return redirect('tasks')
             
-
+@login_required
 def signout(request):
     logout(request)
     return redirect('home')
